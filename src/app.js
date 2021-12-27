0a9e7faf22710fa1cfcd50b3c43ad667a3513f92
test <sli_@daum.net>
2021-12-28 05:09:35 +0900

    backup

--- src/app.js
+++ src/app.js
@@ -2,43 +2,57 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+    console.log('received')
     switch (req.url) {
         case '/home':
             console.log('sending... /home')
-            res.writeHead(200, {'Content-Type' : 'text/html'})
-            res.write('<html><body><div><img src = "this_is_test_image.png"/><img src = "this_is_test_image.png"/><img src = "this_is_test_image.png"/></div></body></html>');
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write('<html><body><div><img src = "/1.png"/><img src = "/2.png"/><img src = "/1.png"/></div> <a href="/users">users</a> <a href="/about">about</a><a href="/redirect">redirect</a></body></html>');
             res.end();
             break;
         case '/about':
             console.log('sending... /about')
-            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write('<html><body><h1>A1234567890<h1><h2>B1234567890<h2></body></html>')
             res.end();
             break;
         case '/redirect':
             console.log('sending... /redirect')
             res.writeHead(302, {
-                'Location':'/about'
+                'Location': '/about'
             });
             res.end()
             break;
         case '/users':
             console.log('sending... /users')
-            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write('<html><body><h1>C1234567890<h1><h2>D1234567890<h2></body></html>')
             res.end();
             break;
 
-        case '/this_is_test_image.png':
-            console.log('sending... /this_is_test_image.png')
-            fs.readFile('./public/this_is_test_image.png', function(err, data) {
-                if(err) throw err;
-                res.writeHead(200, {'Content-Type': 'image/png'});
+        case '/1.png':
+            console.log('sending... /1.png')
+            fs.readFile('./public/1.png', function (err, data) {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'image/png' });
                 res.end(data);
             })
             break;
-    }
-    
-}).listen(8080);
 
-console.log('Navigate to http://localhost:8080/.');
+        case '/2.png':
+            console.log('sending... /2.png')
+            fs.readFile('./public/2.png', function (err, data) {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'image/png' });
+                res.end(data);
+            })
+            break;
+
+        default:
+            res.end()
+            break;
+    }
+
+}).listen(3000);
+
+console.log('Navigate to http://localhost:3000/.');
